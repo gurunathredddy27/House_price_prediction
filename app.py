70f1624ae2a7031c25e989277b1896b14e594f28
gurunathreddy <gurunathreddy2727@gmail.com>
2025-06-20 13:33:18 +0530

    force port 3000

--- app.py
+++ app.py
@@ -82,5 +82,8 @@ def predict():
 
 # if __name__ == "__main__":
 #     app.run(debug=True)
+import os
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    port = int(os.environ.get("PORT", 3000))  # Replit uses PORT env variable internally
+    app.run(host='0.0.0.0', port=port, debug=True)
